e59997ac822762885431d5a6f46bbd961bc14651
Asahi Iwase <asahi.iwase.engineer@gmail.com>
2021-09-09 13:45:55 +0900

    nginx でリクエストを受け付けるようにする

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,11 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
+
+# socket の設定
+app_root = File.expand_path("../..", __FILE__)
+bind "unix://#{app_root}/tmp/sockets/puma.sock"
 
 # Specifies the `environment` that Puma will run in.
 #
